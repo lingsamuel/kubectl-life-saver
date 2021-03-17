6705301b1f2dc5095ce3de58002332e42e2c87c6
Ling Samuel <lingsamuelgrace@gmail.com>
2021-03-17 09:53:28 +0800

    update
    Signed-off-by: Ling Samuel <lingsamuelgrace@gmail.com>

--- generator.sh
+++ generator.sh
@@ -17,16 +17,16 @@ _K8S_COMMAND_LIST=(
   'r/delete'
 )
 _K8S_RESOURCE_LIST=(
-## Cluster Resources List
+  ## Cluster Resources List
   'n/Nodes'
   'ns/Namespaces'
   'l/LimitRanges'
-## RBAC Resources List
+  ## RBAC Resources List
   'crb/ClusterRoleBindings'
   'rb/RoleBindings'
   'cr/ClusterRoles'
   'r/Roles'
-## App Related Resources List
+  ## App Related Resources List
   'ds/DaemonSets'
   'd/Deployments'
   'rs/ReplicaSets'
@@ -47,6 +47,7 @@ _K8S_RESOURCE_LIST=(
   'sa/ServiceAccounts'
   'sc/StorageClasses'
   'np/NetworkPolicies'
+  'ep/Endpoints'
 )
 __generate_k8s_aliases() {
   local OUTPUT=${1:-"$HOME/.k8s_aliases.sh"}
@@ -56,6 +57,16 @@ alias ns='echo $NS'
 alias k='kubectl'
 alias kn='kubectl -n $NS'
 alias ka='kubectl --all-namespaces'
+
+alias kg='kubectl get'
+alias kng='kubectl get -n $NS'
+alias kag='kubectl get --all-namespaces'
+alias ke='kubectl edit'
+alias kne='kubectl edit -n $NS'
+alias kd='kubectl describe'
+alias knd='kubectl describe -n $NS'
+alias kr='kubectl delete'
+alias knr='kubectl delete -n $NS'
 
 # exec and logs doesn't need resource
 alias kx='kubectl exec -it'
@@ -72,10 +83,7 @@ EOF
       rs_shortcut=$(echo "$resource" | awk -F '/' '{print $1}')
       cmd=$(echo "$command" | awk -F '/' '{print $2}')
       rs=$(echo "$resource" | awk -F '/' '{print $2}')
-      echo "alias k${cmd_shortcut}='kubectl $cmd'" >> $OUTPUT
-      echo "alias kn${cmd_shortcut}='kubectl -n \$NS $cmd'" >> $OUTPUT
-      echo "alias ka${cmd_shortcut}='kubectl $cmd --all-namespaces'" >> $OUTPUT
-      echo "alias k${cmd_shortcut}='kubectl $cmd $rs'" >> $OUTPUT
+      echo "alias k${cmd_shortcut}${rs_shortcut}='kubectl $cmd $rs'" >> $OUTPUT
       echo "alias kn${cmd_shortcut}${rs_shortcut}='kubectl -n \$NS $cmd $rs'" >> $OUTPUT
       echo "alias ka${cmd_shortcut}${rs_shortcut}='kubectl $cmd --all-namespaces $rs'" >> $OUTPUT
       echo "" >> $OUTPUT
